6291f2566966c14bba9edf0b854d45848ad77d84
Aaron VonderHaar <gruen0aermel@gmail.com>
2013-06-03 12:55:38 -0700

    Improve output for check_commands.sh

--- .bash_profile_includes/check_commands.sh
+++ .bash_profile_includes/check_commands.sh
@@ -2,6 +2,6 @@
 
 for i in wget git curl gdate mate; do
 	if ! which "$i" >/dev/null; then
-		echo "$0: Command not found on PATH: $i" >&2
+		echo "$BASH_SOURCE: Command not found on PATH: $i" >&2
 	fi
 done
